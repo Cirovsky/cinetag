a9cf6030e28bed689a16a5419eb01206999e834a
Cirovsky <cirovisqui@gmail.com>
2023-04-14 11:50:09 -0300

    finalização do aplicativo

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import styles from './Main.module.css';
 
-export default function Main({title}) {
+export default function Main({title, children}) {
+
     return (
         <main className={styles.mainContainer}>
             <h2>{title}</h2>
+            {children}
         </main>
     )
 }
